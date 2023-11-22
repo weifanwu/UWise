41aa8cb80214466429e405a8cb291186caac4065
weifan_certik <weifan.wu@certik.com>
2023-11-21 23:09:52 -0800

    callback

--- back-end-web/routes/auth/auth.js
+++ back-end-web/routes/auth/auth.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const crypto = require('crypto');
+require('dotenv').config()
 
 const GOOGLE_CLIENT_ID = "73295202240-g4r4fqevidd18jjvoinih26ng5f5cd59.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";

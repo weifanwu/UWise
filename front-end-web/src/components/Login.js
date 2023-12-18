afa781a6f0717a8dd0a7faf90bced678fff35b0a
jasminezhang <jzhang59@uw.edu>
2023-12-18 00:24:37 -0800

    change the log in button

--- front-end-web/src/components/Login.js
+++ front-end-web/src/components/Login.js
@@ -11,7 +11,12 @@ export default function Log(props) {
                 <div><img style={{ marginLeft: "50px",width: "40px", height: "40px" }} src="../images/uwise5.png" className="logo" /><span style={{ font: "50px" }}>UWise 登录</span></div>
                 <button onClick={() => {
                     window.open(host + "/auth/google", "_self");
-                }} style={{ marginTop: "8px",border: "1px solid black", borderRadius: "6px", width: "280px" }} class="btn btn-outline-dark"><img style={{ width: "30px", height: "30px", marginRight: "10px" }} src='../images/google.png'></img>Google Login</button>
+                }} style={{ marginTop: "32px",border: "1px solid black", borderRadius: "6px", width: "280px", height: "54px"}} class="btn btn-dark"><img style={{ width: "32px", height: "32px", marginRight: "10px" }} src='../images/google.png'></img>Google Login</button>
+
+                <button onClick={() => {
+                    window.open(host + "/auth/wechat", "_self");
+                }} style={{ marginTop: "32px", borderRadius: "6px", width: "280px", height: "54px"}} className="custom-btn"><img style={{ width: "36px", height: "36px", marginRight: "20px" }} src='../images/wechat-white.svg'></img>微信登陆</button>
+
             </div>
         </div>
     </>

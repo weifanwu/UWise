06c9b0d4d15b4010b7257ed56bd9871a0d8422b2
weifan_certik <weifan.wu@certik.com>
2023-12-19 15:06:34 -0800

    change the footer

--- front-end-web/src/components/Login.js
+++ front-end-web/src/components/Login.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
+import { message, Typography } from 'antd';
 import "./Login.css";
 export default function Log(props) {
     const host = process.env.REACT_APP_BACKEND_HOST;
-
+    const { Text, Link } = Typography;
     return <>
         <div className={(props.isModalOpen ? " showModal" : "hiddenModal")} onClick={() => {
             props.setIsModalOpen();
@@ -14,9 +15,15 @@ export default function Log(props) {
                 }} style={{ marginTop: "32px",border: "1px solid black", borderRadius: "6px", width: "280px", height: "54px"}} class="btn btn-dark"><img style={{ width: "32px", height: "32px", marginRight: "10px" }} src='../images/google.png'></img>Google Login</button>
 
                 <button onClick={() => {
-                    window.open(host + "/auth/wechat", "_self");
+                    // window.open(host + "/auth/wechat", "_self");
+                    message.success("微信登陆正在开发敬请期待, 请先使用GOOGLE");
                 }} style={{ marginTop: "32px", borderRadius: "6px", width: "280px", height: "54px"}} className="custom-btn"><img style={{ width: "36px", height: "36px", marginRight: "20px" }} src='../images/wechat-white.svg'></img>微信登陆</button>
-
+                <div style={{ marginTop: '160px', textAlign: 'center' }}>
+                    <Text type="secondary">登录或注册即同意</Text>
+                    <Link href="https://ant.design/components/typography">
+                    <Text mark>《UWise 平台用户协议》</Text>
+                    </Link>
+                </div>
             </div>
         </div>
     </>

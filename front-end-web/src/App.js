86d1838eccf2e443e47afa59e4ff5ff556945e1a
weifan_certik <weifan.wu@certik.com>
2023-12-21 11:44:33 -0800

    get

--- front-end-web/src/App.js
+++ front-end-web/src/App.js
@@ -54,7 +54,6 @@ function App() {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
         },
       })
         .then((response) => {

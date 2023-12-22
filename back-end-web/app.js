0abee6997e4a3b3b7cc0ce42a259571e7827a376
weifan_certik <weifan.wu@certik.com>
2023-12-22 14:26:22 -0800

    weifan

--- back-end-web/app.js
+++ back-end-web/app.js
@@ -18,6 +18,7 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('trust proxy', 1)
 
 app.use(logger('dev'));
 app.use(express.json());

77f10238aead4b5eb59d404e168ade1d8038ab06
weifan_certik <weifan.wu@certik.com>
2023-12-23 15:43:37 -0800

    chage

--- back-end-web/app.js
+++ back-end-web/app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [frontend, internal],
+    origin: [frontend, internal, "http://localhost:3000"],
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
     maxAge: 3600,

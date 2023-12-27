ebefa9abc41affda6d225a8c69bf391b35bcbfe6
James Huang <13161133872@163.com>
2023-12-27 15:26:44 -0800

    navbar UI bug fixed

--- front-end-web/src/components/Navbar.js
+++ front-end-web/src/components/Navbar.js
@@ -146,24 +146,24 @@ function Navbar(props) {
               </Link>
             </li>
             <li className='nav-item' data-bs-toggle="dropdown">
-                <Dropdown
-                  dropdownRender={() => (
-                    <div style={contentStyle}>
-                      <Input
-                      onChange={handleChange}
-                      placeholder="请输入激活码" />
-                      <Divider style={{ margin: 0 }} />
-                      <Space style={{ padding: 8 }}>
-                      <Button onClick={handleActivation} type="primary">激活</Button>
-                      </Space>
-                    </div>
-                  )}
-                >
-                <Link
+              <Dropdown
+                dropdownRender={() => (
+                  <div style={contentStyle}>
+                    <Input
+                    onChange={handleChange}
+                    placeholder="请输入激活码" />
+                    <Divider style={{ margin: 0 }} />
+                    <Space style={{ padding: 8 }}>
+                    <Button onClick={handleActivation} type="primary">激活</Button>
+                    </Space>
+                  </div>
+                )}
+              >
+              <Link
                 to='/classes'
                 className='nav-links'
-                >课程</Link>
-                </Dropdown>
+              >课程</Link>
+              </Dropdown>
             </li>
             <li className ='nav-item'>
               <Link

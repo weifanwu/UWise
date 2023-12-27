1177030e2a97be35d9fb097f8711e162a06fedfd
weifan_certik <weifan.wu@certik.com>
2023-12-27 13:06:39 -0800

    debug

--- front-end-web/src/components/pages/Payment.js
+++ front-end-web/src/components/pages/Payment.js
@@ -59,14 +59,14 @@ export default function Payment(props) {
                     <Button style={{ marginTop: 30, marginLeft: 300 }} onClick={() => {
                     if (!props.userInfo) {
                         props.setIsModalOpen(!props.isModalOpen)
-                    // } else if (!value.includes(currentClass)) {
-                    //     message.error({
-                    //         content: <div>
-                    //             <p>请联系小助手买课</p>
-                    //             <img style={{ width: "200px" }} src="/images/小助手.jpeg"></img>
-                    //         </div>,
-                    //         duration: 4,
-                    //       });
+                    } else if (!value.includes(currentClass)) {
+                        message.error({
+                            content: <div>
+                                <p>请联系小助手买课</p>
+                                <img style={{ width: "200px" }} src="/images/小助手.jpeg"></img>
+                            </div>,
+                            duration: 4,
+                          });
                     } else {
                         navigate('/class/jOTfBlKSQYY');
                     }

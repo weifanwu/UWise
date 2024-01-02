90bb45785c866cd5052127431b7215be4588d00d
weifan_certik <weifan.wu@certik.com>
2024-01-01 18:43:58 -0800

    en

--- front-end-web/src/App.js
+++ front-end-web/src/App.js
@@ -88,9 +88,9 @@ function App() {
           <Route path='/' exact element={<Home />} />
           <Route path='/classes' element={<Services />} />
           <Route path='/reviews' element={<Products />} />
-          <Route path='/class/:id' element={<ProtectedRoute classname={name}><ClassHome classname={name} /></ProtectedRoute>} />
-          <Route path='/payment' element={<Payment isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} courseName={name} userInfo={profile} classNames={classNames} />} />
-          <Route path="/video/:videoId" element={<VideoDisplay courseName={name}/>} />
+          <Route path='/class/:id' element={<ProtectedRoute ><ClassHome /></ProtectedRoute>} />
+          <Route path='/payment' element={<Payment isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} userInfo={profile} classNames={classNames} />} />
+          <Route path="/video/:videoId" element={<VideoDisplay/>} />
           <Route path='/resources' element={<Student />} />
           <Route path='/map' element={<ResourceMap />} />
           <Route path='/profile' element={<Profile />} />

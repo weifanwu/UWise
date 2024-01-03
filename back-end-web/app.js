c8760508e62052908006ac976a9cf30bec317654
w-jiaqi <102758409+w-jiaqi@users.noreply.github.com>
2024-01-02 18:10:59 -0800

    update

--- back-end-web/app.js
+++ back-end-web/app.js
@@ -53,7 +53,7 @@ app.use('/resources', staticResources);
 app.get('/test', (req, res) => {
   res.status(200).json({
     success: true,
-    message: "this is 2024",
+    message: "01/02/2024",
     user: req.user,
   })});
 

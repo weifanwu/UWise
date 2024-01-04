864a0fe239a315f00d430e25643cbdc826e88173
weifan_certik <weifan.wu@certik.com>
2024-01-03 23:41:13 -0800

    remove dead code

--- back-end-web/routes/database/redis.js
+++ back-end-web/routes/database/redis.js
@@ -1,13 +1,8 @@
 var express = require('express');
-const { connect } = require('mongoose');
 var router = express.Router();
 const redis = require('redis');
 const User = require('../../models/userSchema.js');
 const generateCode = require('../../utils/generateCode.js');
-
-function isLoggedIn(req, res, next) {
-    req.user ? next() : res.sendStatus(401);
-  }
 
   const valid_classes = ["AMATH301", "ECON200", "ECON201", "MATH124", "MATH125", "CSE122" ,"CHEM142", "Math208"];
   router.post('/buyClass', async (req, res) => {

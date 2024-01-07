6d8c44545d40f6ab714db326cca76e00c29e2e26
weifan_certik <weifan.wu@certik.com>
2024-01-06 22:37:42 -0800

    add

--- front-end-web/src/components/pages/Payment.js
+++ front-end-web/src/components/pages/Payment.js
@@ -6,7 +6,7 @@ import Lectures from "../LectureCard";
 
 export default function Payment(props) {
     const currentClass = localStorage.getItem("currentClass");
-    const classToName = {"ECON200": "Francis", "ECON201": "黄诗培"};
+    const classToName = {"ECON200": "Francis", "ECON201": "shipei"};
     const items = [
         {
           key: '1',

c71cca335b712a974666a24d490514fa78c485c4
jerryiscat <94934251+jerryiscat@users.noreply.github.com>
2024-01-19 10:35:32 -0800

    update to non-edit version

--- front-end-web/src/components/Navbar.js
+++ front-end-web/src/components/Navbar.js
@@ -62,6 +62,7 @@ function Navbar(props) {
   useEffect(() => {
     dispatch(incrementAsync());
     document.addEventListener('dblclick', handleClickOutside);
+    //dblclick
 
     return () => {
       document.removeEventListener('dblclick', handleClickOutside);
@@ -209,7 +210,7 @@ function Navbar(props) {
                   />
                   {showProfilePopup && (
                     <div className="profile-popup" ref={profilePopupRef}>
-                      <Profile oldProfileInfo={profileInfo ? profileInfo: ""} email={ profileInfo ? profileInfo.email : props.email} picture={profileInfo ? profileInfo.picture : props.picture}/>
+                      <Profile oldProfileInfo={profileInfo ? profileInfo: ""} email={props.email} picture={props.picture}/>
                     </div>
                   )}
                 </>

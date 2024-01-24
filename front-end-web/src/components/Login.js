a7d0f26b731ac0a00567df94f609ef8fd81f9442
jerryiscat <94934251+jerryiscat@users.noreply.github.com>
2024-01-24 15:57:18 -0800

    change the nav and the home page video image

--- front-end-web/src/components/Login.js
+++ front-end-web/src/components/Login.js
@@ -12,7 +12,7 @@ export default function Log(props) {
                 <div><img style={{ marginLeft: "50px",width: "40px", height: "40px" }} src="../images/uwise5.png" className="logo" /><span style={{ font: "50px" }}>UWise 登录</span></div>
                 <button onClick={() => {
                     window.open(host + "/auth/google", "_self");
-                }} style={{ marginTop: "32px",border: "1px solid black", borderRadius: "6px", width: "280px", height: "54px"}} class="btn btn-dark"><img style={{ width: "32px", height: "32px", marginRight: "10px" }} src='../images/google.png'></img>Google Login</button>
+                }} style={{ marginTop: "32px",border: "1px solid black", borderRadius: "6px", width: "260px", height: "54px"}} class="btn btn-dark"><img style={{ width: "32px", height: "32px", marginRight: "10px" }} src='../images/google.png'></img>Google Login</button>
 
                 <button onClick={() => {
                     window.open(host + "/auth/wechat", "_self");

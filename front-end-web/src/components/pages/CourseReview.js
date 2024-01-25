8c9ed5c5dd8dcbe4e1a1e7b824b50bfe0ebce7aa
jerryiscat <94934251+jerryiscat@users.noreply.github.com>
2024-01-25 12:44:45 -0800

    update the static resources

--- front-end-web/src/components/pages/CourseReview.js
+++ front-end-web/src/components/pages/CourseReview.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../App.css';
-import './Services.css';
-export default function Products() {
+import './CourseReview.css';
+import Footer from '../Footer';
+
+export default function CourseReview() {
   return <>
             <body>
                 <iframe className="service" src="https://uwise-course-review.netlify.app/#/" frameborder="0"></iframe>
             </body>
-            <footer>
-            </footer>
+            {/* <Footer /> */}
          </>
 }

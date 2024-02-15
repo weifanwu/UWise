2dbe45802c521a2a110537f4c3e188a83a676454
weifan_certik <weifan.wu@certik.com>
2024-02-14 22:44:01 -0800

    comment out the visit stats

--- front-end-web/src/components/Footer.js
+++ front-end-web/src/components/Footer.js
@@ -42,12 +42,12 @@ function Footer(props) {
         <div class='website-rights'>
             <small>UWise华大优智 © 2024</small>
         </div>
-        <div style={{ display: viewers.includes(props.email) ? "block" : "none", color: 'white' }}>
+        {/* <div style={{ display: viewers.includes(props.email) ? "block" : "none", color: 'white' }}>
             <p>总访问量: <span id="qiushaocloud_sitecounter_value_site_pv">n</span></p>
             <p>总访客量: <span id="qiushaocloud_sitecounter_value_site_uv">n</span></p>
             <p>今日访问量: <span id="qiushaocloud_sitecounter_value_today_site_pv">n</span></p>
             <p>今日访客量: <span id="qiushaocloud_sitecounter_value_today_site_uv">n</span></p>
-        </div>
+        </div> */}
     </div>
   );
 }

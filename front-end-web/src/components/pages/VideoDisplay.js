6045f8c0bd4eae862e4422710193834036901904
Allan Ji <zihej2@cs.washington.edu>
2024-02-27 17:20:24 -0800

    Embed the videoplayer to the web

--- front-end-web/src/components/pages/VideoDisplay.js
+++ front-end-web/src/components/pages/VideoDisplay.js
@@ -1,96 +1,135 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './VideoDisplay.css';
-import { useLocation, Link} from 'react-router-dom';
+import React, { useEffect, useState, useRef } from "react";
+import "./VideoDisplay.css";
+import { useLocation, Link } from "react-router-dom";
 
 function VideoDisplay(props) {
-    const location = useLocation();
-    const { currentLecture, lectures } = location.state || {};
-    const [videoUrl, setVideoUrl] = useState("");
+  const location = useLocation();
+  const { currentLecture, lectures } = location.state || {};
+  const [videoUrl, setVideoUrl] = useState("");
 
-    const host = process.env.REACT_APP_BACKEND_HOST;
-    const handlePlayButtonClick = () => {
-        fetch(`${host}/courses/getVideo?courseName=${encodeURIComponent(currentLecture.courseName)}&lecture=${encodeURIComponent(currentLecture.title)}`)
-            .then((res) => res.json())
-            .then((url) => {
-                const iframe = document.getElementById("videoIframe");
-                setVideoUrl(url[0]);
-                iframe.style.display = 'block';
-                const VideoPlayButtonClick = document.getElementById("video-play-btn");
-                VideoPlayButtonClick.style.display = 'none';
-                const cover = document.getElementById("cover");
-                cover.style.display = 'none';
-            })
+  const host = process.env.REACT_APP_BACKEND_HOST;
+
+  //   const videoUrl = `${host}/courses/getVideo?courseName=${encodeURIComponent(
+  //     currentLecture.courseName
+  //   )}&lecture=${encodeURIComponent(currentLecture.title)}`;
+
+  //   const handlePlayButtonClick = () => {
+  //     fetch(
+  //       `${host}/courses/getVideo?courseName=${encodeURIComponent(
+  //         currentLecture.courseName
+  //       )}&lecture=${encodeURIComponent(currentLecture.title)}`
+  //     )
+  //       .then((res) => res.json())
+  //       .then((url) => {
+  //         const iframe = document.getElementById("videoIframe");
+  //         setVideoUrl(url[0]);
+  //         iframe.style.display = "block";
+  //         const VideoPlayButtonClick = document.getElementById("video-play-btn");
+  //         VideoPlayButtonClick.style.display = "none";
+  //         const cover = document.getElementById("cover");
+  //         cover.style.display = "none";
+  //       });
+  //   };
+
+  const handlePlayButtonClick = () => {
+    const videoUrl = `${host}/courses/getVideo?courseName=${encodeURIComponent(
+      currentLecture.courseName
+    )}&lecture=${encodeURIComponent(currentLecture.title)}`;
+
+    setVideoUrl(videoUrl);
+
+    const videoElement = document.getElementById("videoIframe");
+    videoElement.src = videoUrl;
+    videoElement.style.display = "block";
+
+    const cover = document.getElementById("cover");
+    cover.style.display = "none";
+
+    const videoPlayButton = document.getElementById("video-play-btn");
+    videoPlayButton.style.display = "none";
+  };
+
+  useEffect(() => {
+    const cover = document.getElementById("cover");
+    cover.style.display = "block";
+    var iframe = document.getElementById("videoIframe");
+    iframe.style.display = "none";
+    setVideoUrl("");
+    const VideoPlayButtonClick = document.getElementById("video-play-btn");
+    VideoPlayButtonClick.style.display = "block";
+  }, [currentLecture]);
+
+  useEffect(() => {
+    const handleContextmenu = (e) => {
+      e.preventDefault();
     };
-
-    useEffect(() => {
-        const cover = document.getElementById("cover");
-        cover.style.display = 'block';
-        var iframe = document.getElementById('videoIframe');
-        iframe.style.display = 'none'; 
-        setVideoUrl("");
-        const VideoPlayButtonClick = document.getElementById("video-play-btn");
-        VideoPlayButtonClick.style.display = 'block';
-
-    }, [currentLecture]);
-
-    useEffect(() => {
-        const handleContextmenu = e => {
-            e.preventDefault()
-        }
-        document.addEventListener('contextmenu', handleContextmenu)
-        return function cleanup() {
-            document.removeEventListener('contextmenu', handleContextmenu)
-        }
-    }, [])
+    document.addEventListener("contextmenu", handleContextmenu);
+    return function cleanup() {
+      document.removeEventListener("contextmenu", handleContextmenu);
+    };
+  }, []);
 
   const videoRef = useRef();
 
-  useEffect(() => {    
+  useEffect(() => {
     videoRef.current?.load();
   }, [videoUrl]);
 
+  const filteredLectures = lectures.filter(
+    (lecture) => lecture.title !== currentLecture?.title
+  );
+  return (
+    <div className="video-layout">
+      <div className="video-detail">
+        <div className="video-detail-image-wrapper">
+          <img id="cover" src={currentLecture?.videoCover} alt="Video Cover" />
+          <video
+            id="videoIframe"
+            allow="fullscreen"
+            frameBorder="0"
+            ref={videoRef}
+            controls
+            controlsList="nodownload"
+          >
+            <source id="video-source" src={videoUrl} />
+          </video>
 
-    const filteredLectures = lectures.filter(lecture => lecture.title !== currentLecture?.title);
-    return (
-        <div className="video-layout">
-                <div className="video-detail">
-                    <div className="video-detail-image-wrapper">
-                        <img id='cover' src={currentLecture?.videoCover} alt="Video Cover" />
-                        <video
-                            id="videoIframe"
-                            allow="fullscreen"
-                            frameBorder="0"
-                            ref={videoRef}                            
-                            controls
-                            controlsList="nodownload"
-                        >
-                            <source id="video-source" src={videoUrl} />
-                        </video>
-
-                        <div id="video-play-btn" className="play-button" onClick={handlePlayButtonClick}>▶</div>
-                    </div>
-                    <div style={{ marginTop: '80px' }}>
-                        <h3>{currentLecture.courseName} {currentLecture?.title}</h3>
-                        {currentLecture.notesUrl ? <a href={currentLecture.notesUrl}>查看笔记</a> : null}
-                    </div>
-                </div>
-                
-                <div className="lectures-list">
-                    {filteredLectures.map((lecture, index) => (
-                        <div key={lecture._id} className="lecture-item">
-                            <Link to={`/video/${lecture._id}`} 
-                              state={{ currentLecture: lecture, lectures: lectures }}
-                              id="lecture-video-link">
-                                <img src={lecture.videoCover} alt="Lecture Cover" />
-                                <div className="lecture-title">
-                                    {lecture.courseName} {lecture.title}
-                                </div>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
+          <div
+            id="video-play-btn"
+            className="play-button"
+            onClick={handlePlayButtonClick}
+          >
+            ▶
+          </div>
         </div>
-    );
+        <div style={{ marginTop: "80px" }}>
+          <h3>
+            {currentLecture.courseName} {currentLecture?.title}
+          </h3>
+          {currentLecture.notesUrl ? (
+            <a href={currentLecture.notesUrl}>查看笔记</a>
+          ) : null}
+        </div>
+      </div>
+
+      <div className="lectures-list">
+        {filteredLectures.map((lecture, index) => (
+          <div key={lecture._id} className="lecture-item">
+            <Link
+              to={`/video/${lecture._id}`}
+              state={{ currentLecture: lecture, lectures: lectures }}
+              id="lecture-video-link"
+            >
+              <img src={lecture.videoCover} alt="Lecture Cover" />
+              <div className="lecture-title">
+                {lecture.courseName} {lecture.title}
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default VideoDisplay;
--- front-end-web/src/components/pages/VideoPlayer.js
+++ front-end-web/src/components/pages/VideoPlayer.js
@@ -1,66 +1,9 @@
-// import React, { useState } from "react";
-
-// const VideoPlayer = () => {
-//   const [videoURL, setVideoURL] = useState("");
-//   const [error, setError] = useState("");
-
-//   const fetchVideo = () => {
-//     const host = process.env.REACT_APP_BACKEND_HOST;
-//     fetch(`${host}/courses/test`, {
-//       // fetch(`http://localhost:8000/`, {
-//       headers: {
-//         Range: "bytes=0-",
-//       },
-//     })
-//       .then((response) => response.blob())
-//       .then((blob) => {
-//         // Create a blob URL pointing to the video object
-//         const url = URL.createObjectURL(blob);
-//         console.log("successfully get the video url:" + url);
-//         setVideoURL(url);
-//         setError(""); // Reset error state
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         setError("Failed to load video.");
-//       });
-//   };
-
-//   const handleVideoError = () => {
-//     setError("Error playing video. Please try again.");
-//   };
-
-//   return (
-//     <div
-//       id="video-container"
-//       style={{ width: "720px", maxHeight: "405px", overflow: "hidden" }}
-//     >
-//       {videoURL ? (
-//         <video
-//           src={videoURL}
-//           controls
-//           style={{ width: "100%" }}
-//           onError={handleVideoError}
-//           autoPlay
-//         >
-//           Your browser does not support the video tag.
-//         </video>
-//       ) : (
-//         <button
-//           onClick={fetchVideo}
-//           style={{ padding: "10px 20px", fontSize: "16px" }}
-//         >
-//           Play Video
-//         </button>
-//       )}
-//       {error && <p style={{ color: "red" }}>{error}</p>}
-//     </div>
-//   );
-// };
-
-// export default VideoPlayer;
-
 import React from "react";
+
+/*
+This is the toy implementation for the video player. Only used for testing purposes.
+TODO: Remove this component when the real video player is implemented.
+*/
 
 const VideoPlayer = () => {
   const host = process.env.REACT_APP_BACKEND_HOST;
@@ -70,7 +13,7 @@ const VideoPlayer = () => {
       <video controls autoPlay style={{ width: "100%" }}>
         <source
           //   src={`http://localhost:8000/courses/testVideo`}
-          src={`${host}/courses/testVideo`}
+          src={`${host}/courses/getVideo`}
           type="video/mp4"
         />
         Your browser does not support the video tag.

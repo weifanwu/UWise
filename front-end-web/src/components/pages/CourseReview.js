9bdfcba97fff2ea24488fe30be80bd1ea510da8c
Allan Ji <zihej2@cs.washington.edu>
2024-05-04 15:30:30 -0700

    Finish creating basic frontend framework

--- front-end-web/src/components/pages/CourseReview.js
+++ front-end-web/src/components/pages/CourseReview.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "../../App.css";
 import "./CourseReview.css";
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  Select,
+  Upload,
+  Card,
+  message,
+} from "antd";
 
 function CourseReview() {
   let { courseName } = useParams();
+  const navigate = useNavigate();
 
-  const host = process.env.REACT_APP_BACKEND_HOST;
+  const frontendHost = process.env.REACT_APP_FRONTEND_HOST;
+  const backendHost = process.env.REACT_APP_BACKEND_HOST;
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
@@ -26,7 +38,11 @@ function CourseReview() {
   // };
 
   const getDescription = () => {
-    fetch(host + "/courseReview/getCourseDescription?courseName=" + courseName)
+    fetch(
+      backendHost +
+        "/courseReview/getCourseDescription?courseName=" +
+        courseName
+    )
       .then((response) => {
         return response.json();
       })
@@ -39,7 +55,9 @@ function CourseReview() {
   };
 
   const getReviews = () => {
-    fetch(host + "/courseReview/getReviewsForCourse?courseName=" + courseName)
+    fetch(
+      backendHost + "/courseReview/getReviewsForCourse?courseName=" + courseName
+    )
       .then((response) => {
         return response.json();
       })
@@ -53,17 +71,54 @@ function CourseReview() {
       });
   };
 
+  const handleShare = () => {
+    navigator.clipboard.writeText(window.location.href);
+    message.success("已复制到剪贴板");
+  };
+
+  const handleBack = () => {
+    navigate("/reviews");
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1>
       <h2>{courseName}</h2>
 
-      <h2>{description}</h2>
+      <h2>{description}</h2> */}
 
       {/* Iterate through the reviews and show the stringified json */}
-      {reviews.map((review) => (
+      {/* {reviews.map((review) => (
         <p>{JSON.stringify(review)}</p>
-      ))}
+      ))} */}
+
+      <div className="App">
+        <header className="main-header">
+          <button onClick={handleBack} className="left-button">
+            返回主页
+          </button>
+          <h1>{courseName}</h1>
+          <button onClick={handleShare} className="right-button">
+            分享
+          </button>
+        </header>
+        <section className="course-description">
+          <h2>介绍</h2>
+          <p>{description}</p>
+        </section>
+        <section className="course-review">
+          <h2>课程评价</h2>
+          <p>{JSON.stringify(reviews)}</p>
+        </section>
+        <form className="review-form">
+          <h2>添加评价</h2>
+          <label>
+            Please rate the course:
+            <input type="range" min="1" max="5" />
+          </label>
+          <button type="submit">Submit Feedback</button>
+        </form>
+      </div>
     </>
   );
 }
